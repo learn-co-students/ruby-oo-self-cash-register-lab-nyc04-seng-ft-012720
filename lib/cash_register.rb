9f371f07cf18b5e0ef504ba6dcfcd63fccd643f5
abidtalukdar <abiditalukdar@gmail.com>
2020-01-28 23:13:58 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ class CashRegister
 
     def apply_discount
         if @discount != 0
-            @total -= (@total * (@discount.to_f)/100).to_i
+            self.total = @total - (@total * (@discount.to_f)/100).to_i
             return "After the discount, the total comes to $#{@total}."
         else
             @total
